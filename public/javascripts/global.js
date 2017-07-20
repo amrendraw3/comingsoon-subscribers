d31b8114beed0d0137ce3f73fcfb70c25d995a00
Amrendra Singh <amrendra.singh@w3villa.com>
2017-07-20 15:14:58 +0530

    Added logo on website

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -6,7 +6,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 */
 $( function() {
 // Add background image
-	$.backstretch('images/hr_gardenbridge.jpg');
+	$.backstretch('images/pexels-photo-390659.jpeg');
 	var endDate = "August 14, 2017 23:59:59";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
