54103459b09339463c2025bb937cf6f7f6a78d25
Amrendra Singh <amrendra.singh@w3villa.com>
2017-08-15 18:02:55 +0530

    Timer extended by one week

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 $( function() {
 // Add background image
 	$.backstretch('images/SIX38 LOGO GOLD BACKGROUND .jpg');
-	var endDate = "August 14, 2017 23:59:59";
+	var endDate = "August 21, 2017 23:59:59";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
 	  date: endDate,
